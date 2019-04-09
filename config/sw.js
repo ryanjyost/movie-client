d634f5adec9eae0202bd4eb3e4aa51802e396586
Ryan Yost <ryost217@gmail.com>
2019-04-08 21:21:06 -0500

    PWA stuff

---
+++ config/sw.js
@@ -0,0 +1,8 @@
+// config/sw.js
+module.exports = {
+  staticFileGlobs: ["build/**/*.js", "build/**/*.css", "build/index.html"],
+  navigateFallback: "/index.html",
+  // something like this should allow everything but files ending with `.zip`
+  navigateFallbackWhitelist: [/^(?!.*[.]zip$).*$/],
+  cacheId: "my-magical-cache-machine"
+};
